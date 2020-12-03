0ad9661ff9fb070b9e2c808630c470c5d7d887ac
Krzysztof Banasik <krzysztof.f.banasik@gmail.com>
2020-12-03 14:42:01 +0100

    Day 3

--- src/main/java/com/github/krisbanas/Main.java
+++ src/main/java/com/github/krisbanas/Main.java
@@ -1,9 +1,9 @@
 package com.github.krisbanas;
 
-import com.github.krisbanas.solutions.Day2;
+import com.github.krisbanas.solutions.Day3;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Day2().solvePartTwo());
+        System.out.println(new Day3().solve());
     }
 }
---
+++ src/main/java/com/github/krisbanas/solutions/Day3.java
@@ -0,0 +1,39 @@
+package com.github.krisbanas.solutions;
+
+import com.github.krisbanas.util.FileHelper;
+
+import java.util.List;
+
+public class Day3 {
+
+    private final List<String> landscape;
+    private final List<int[]> parameterSetup;
+
+    public Day3() {
+        landscape = FileHelper.loadFileFromResources("Day3Input.txt");
+        parameterSetup = List.of(
+                new int[]{1, 1},
+                new int[]{3, 1},
+                new int[]{5, 1},
+                new int[]{7, 1},
+                new int[]{1, 2}
+        );
+    }
+
+    public long solve() {
+        return parameterSetup.stream()
+                .map(parameters -> getTreeCountForSlopeParams(parameters[0], parameters[1]))
+                .reduce(1L, (a, b) -> a * b);
+    }
+
+    private long getTreeCountForSlopeParams(int widthSkip, int heightSkip) {
+        var count = 0;
+        var height = landscape.size();
+        var width = landscape.get(0).length();
+        for (int i = 0; i < height; i++) {
+            if (heightSkip * i >= height) break;
+            if (landscape.get(heightSkip * i).charAt((widthSkip * i) % width) == '#') count++;
+        }
+        return count;
+    }
+}
